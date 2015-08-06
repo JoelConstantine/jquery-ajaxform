b24e5b9eac41948b9b40d187a9018afe15126461
Joel Constantine <joel@joelconstantine.com>
2015-08-06 18:43:50 -0500

    Added gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			build: {
+				files: {
+					'jquery.ajaxForm.min.js': ['jquery.ajaxForm.js']
+				}
+			}
+		},
+    jshint: {
+      js: {
+        all: ['src/jquery.ajaxForm.js']
+      }
+    },
+    concat: {
+      build: {
+        src: ['src/*.js'],
+        dest: 'jquery.ajaxForm.js'
+      }
+    },
+    watch: {
+      js: {
+        files: ['src/*.js'],
+        tasks: ['jsBuild'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['watch']);
+  grunt.registerTask('jsBuild', ['jshint', 'concat', 'uglify'])
+};
