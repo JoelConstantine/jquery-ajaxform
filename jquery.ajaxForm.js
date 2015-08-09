2f5f7e3e79ea8cb4c6ad208e318d6506a54c07d8
Joel Constantine <joel@joelconstantine.com>
2015-08-09 13:10:21 -0500

    Began adding in methods for init

--- jquery.ajaxForm.js
+++ jquery.ajaxForm.js
@@ -1,5 +1,39 @@
-(function($) {
-	$.fn.ajaxForm = function() {
-		
-	}
-}(jQuery));
+(function (factory) {
+  if ( typeof define === "function" && define.amd ) {
+    define( ["jquery"], factory );
+  } else {
+    factory( jQuery );
+  }
+}(function($) {
+	'use strict';
+
+  // Add the ajaxForm method to the jQuery object
+	$.extend($.fn, {
+    ajaxForm: function (options) {          
+      if(!options) { options = {}; }
+
+      if ( !this.length) {
+        return false;
+      }
+
+      var form = $.data(this[0], "ajaxform");
+      if (form) {
+        return form;
+      }
+
+      form = new $.ajaxform( options, this [0]);
+      return form;
+    }
+  });
+
+  //Add function to initialize options
+  $.ajaxform = function ( options, form ) {
+    this.settings = $.extend( true, {}, $.ajaxform.defaults, options );
+    this.currentForm = form;
+    this.init();
+  };
+  
+  
+
+
+}));
